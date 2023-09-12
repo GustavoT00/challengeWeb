e5051cf9890c67185419495c7436fc027771320a
GustavoT00 <gustavo_ale.tovar@uao.edu.co>
2023-09-12 15:21:19 -0500

    FirstApp Component

---
+++ src/FirstApp.jsx
@@ -0,0 +1,14 @@
+const title = "First App"
+
+const FirstApp = () => {
+    return (
+      <>
+        <h1>Hola</h1>
+        <h2>{title}</h2>
+        <span><text>10</text></span>
+      </>
+    )
+
+}
+
+export default FirstApp
